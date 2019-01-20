3bfbd8e8a8bce319df69d54e14cc132c7aea7a9c
dingfengqian <1079874473@qq.com>
2019-01-20 17:54:27 +0800

    test new laptop test hddm_w version 4

--- local_tests/drift_detection/arbitrage_tst.py
+++ local_tests/drift_detection/arbitrage_tst.py
@@ -1,10 +1,11 @@
-import matlab.engine
+#import matlab.engine
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.optimize import fmin
 from arch import arch_model
-
+from hddm_w import HDDM_W
+from sklearn.preprocessing import MinMaxScaler
 
 def calStaArbitrageParam(preDataA, preDataB):
     print(np.shape(preDataA))
@@ -28,7 +29,7 @@ def draw(data, title):
     ax.set_title(title, fontdict={'family' : 'SimHei'})
     plt.show()
 
-def draw(data1, data2,title1, title2):
+def draw2(data1, data2,title1, title2):
     fig = plt.figure()
     ax1 = fig.add_subplot(211)
     ax1.plot(range(len(data1)), data1)
@@ -38,14 +39,50 @@ def draw(data1, data2,title1, title2):
     ax2.set_title(title2, fontdict={'family' : 'SimHei'})
     plt.show()
 
+def showPlot(data, data2):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(range(len(data)), data)
+    ax.plot(range(len(data2)), data2)
+    plt.legend(['acc','bound'])
+    plt.show()
+
+
+def hddm_w_tst(spread):
+    hddm = HDDM_W()
+    data_stream = spread
+
+    average_prediciton = []
+    average_prediction_bound = []
+    for i in range(len(spread)):
+        hddm.add_element(data_stream[i])
+        if hddm.detected_warning_zone():
+            print('Warning zone has been detected in data: ' + str(data_stream[i]) + ' - of index: ' + str(i))
+        if hddm.detected_change():
+            print('Change has been detected in data: ' + str(data_stream[i]) + ' - of index: ' + str(i))
+        average_prediciton.append(hddm.Z_)
+        average_prediction_bound.append(hddm.Z_epsilon)
+        # print('X', hddm.X_, ' ', 'Z', hddm.Z_)
+        # print('X_e', hddm.X_epsilon, ' ', 'Z', hddm.Z_epsilon)
+    showPlot(average_prediciton, average_prediction_bound)
+
+
 if __name__ == '__main__':
 
-    df_A = pd.read_csv('./CloseA.csv', header=None).as_matrix().astype(np.double)
-    df_B = pd.read_csv('./CloseB.csv', header=None).as_matrix().astype(np.double)
+    df_A = pd.read_csv('./CloseA.csv', header=None).values.astype(np.double)
+    df_B = pd.read_csv('./CloseB.csv', header=None).values.astype(np.double)
 
     start = 1
-    window = 100
+    window = 300
     #result = calStaArbitrageParam(df_A[267:367], df_B[267:367])
     spread, mspread = calStaArbitrageParam(df_A[start:window+start], df_B[start:window+start])
     #draw(mspread, '去中心化残差')
-    draw(spread, mspread, '残差', '去中心化残差')
+    #draw2(spread, mspread, '残差', '去中心化残差')
+    print(mspread)
+
+    mm = MinMaxScaler()
+    result = mm.fit_transform(mspread)
+    #draw(result,'归一化残差')
+    #draw2(mspread, result, '去中心化残差','归一化去中心化残差')
+    print(result)
+    hddm_w_tst(spread)
